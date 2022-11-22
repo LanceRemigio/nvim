5d280874a8689cdf2c285f5e28e9ea4680ed5138
LanceRemigio <102345313+LanceRemigio@users.noreply.github.com>
2022-11-21 16:30:19 -0800

    init.lua
    General settings for neovim

---
+++ settings/init.lua
@@ -0,0 +1,14 @@
+local set = vim.opt
+
+set.number = true 
+set.relativenumber = true 
+set.mouse = 'a'
+set.autoindent = true 
+set.tabstop = 4 
+set.shiftwidth = 4 
+set.smarttab = true 
+set.softtabstop = 4 
+set.termguicolors = true 
+set.cursorline = true 
+set.expandtab = true 
+set.modifiable = true 
