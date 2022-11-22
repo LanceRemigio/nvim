022c3a02497dc3bbc505503e343ced9599d21d74
LanceRemigio <102345313+LanceRemigio@users.noreply.github.com>
2022-11-21 17:39:13 -0800

    vimtex.lua

---
+++ lua/vimtex/init.lua
@@ -0,0 +1,9 @@
+local let = vim.g
+
+let.cmd = [['filetype plugin indent on']]
+
+let.cmd = [['syntax enable']]
+
+let.vimtex_view_method = 'zathura'
+-- let.vimtex_quickfix_mode = 0
+vim.opt.conceallevel = 1
