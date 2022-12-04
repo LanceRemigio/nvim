4ce90b93b6134914c9cc3bdc033695e98bed0e9b
LanceRemigio <102345313+LanceRemigio@users.noreply.github.com>
2022-12-03 22:08:32 -0800

    Update init.lua

--- lua/packer-plugins/init.lua
+++ lua/packer-plugins/init.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function()
     -- Functionality
     use 'wbthomason/packer.nvim'
@@ -23,4 +36,7 @@ return require('packer').startup(function()
     use 'Pocco81/auto-save.nvim'
     use 'SirVer/ultisnips'
     use 'lervag/vimtex'
+ if packer_bootstrap then
+    require('packer').sync()
+ end
 end)
