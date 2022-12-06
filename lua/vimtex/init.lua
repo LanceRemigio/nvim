ba0dcb3e9395ac4e1ecd2411ac221d91e4066405
LanceRemigio <102345313+LanceRemigio@users.noreply.github.com>
2022-12-05 23:49:31 -0800

    Delete lua directory

--- lua/vimtex/init.lua
+++
@@ -1,9 +0,0 @@
-local let = vim.g
-
-let.cmd = [['filetype plugin indent on']]
-
-let.cmd = [['syntax enable']]
-
-let.vimtex_view_method = 'zathura'
--- let.vimtex_quickfix_mode = 0
-vim.opt.conceallevel = 1
