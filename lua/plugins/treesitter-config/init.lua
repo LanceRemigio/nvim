dad3c80b8517187f9d16c0d8d969dc1ca97a95ec
LanceRemigio <102345313+LanceRemigio@users.noreply.github.com>
2022-12-06 09:43:46 -0800

    Create init.lua

---
+++ lua/plugins/treesitter-config/init.lua
@@ -0,0 +1,8 @@
+require('nvim-treesitter.configs').setup {
+    ensure_installed = {'latex', 'python', 'javascript'},
+    highlight = { 
+        enable = true,
+        disable = {'latex'},
+    },
+
+}
