37858443dcc3607a780e5b2649c509386987f365
LanceRemigio <102345313+LanceRemigio@users.noreply.github.com>
2022-12-06 09:24:29 -0800

    Update init.lua

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -1,1 +1,20 @@
 
+local set = vim.opt
+local let = vim.g
+
+
+let.cmd = [['filetype plugin indent on']]
+let.cmd = [['syntax enable']]
+
+set.number = true 
+set.relativenumber = true 
+set.mouse = 'a'
+set.autoindent = true 
+set.tabstop = 4 
+set.shiftwidth = 4 
+set.smarttab = true 
+set.softtabstop = 4 
+set.termguicolors = true 
+set.cursorline = true 
+set.expandtab = true 
+set.modifiable = true 
